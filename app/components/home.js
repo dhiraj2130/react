46c705f6bb31fe65b8860252338ab6dca6151401
Dhiraj Kumar <dhiraj.kumar@comsydmac10.complispace.local>
2016-12-14 10:32:20 +1100

    first component

---
+++ app/components/home.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dhiraj.kumar on 14/12/2016.
+ */
+var React = require('react');
+
+var Home = React.createClass({
+    render: function(){
+        return (
+            <h2 className="text-center" >
+                search by usernae Above
+            </h2>
+        )
+    }
+})
